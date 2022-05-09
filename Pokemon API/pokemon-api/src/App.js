1b79e0077609dfaab45c9ca666d7181cb0a58316
lundrew <andrueee@gmail.com>
2022-05-09 15:33:26 -0700

    Co-authored-by: Cj <cjpriedel@users.noreply.github.com>

--- Pokemon API/pokemon-api/src/App.js
+++ Pokemon API/pokemon-api/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, {Component} from 'react'
-import pokedex from './'
+  
 
 
 export default class App extends Component {
@@ -8,7 +8,9 @@ export default class App extends Component {
     super(props)
     this.state = {
       ipPayload: "",
-      imageURL: ""
+      imageURL: "",
+      pokemonType: "",
+      identification: ""
     }
   }
 
@@ -16,7 +18,7 @@ getPokemon = () => {
   let input = prompt()
   fetch("https://pokeapi.co/api/v2/pokemon/" + input)
   .then(response => (response.json())
-  .then(payload => this.setState({ipPayload: payload, imageURL: payload.sprites.other.dream_world.front_default})))
+  .then(payload => this.setState({ipPayload: payload, imageURL: payload.sprites.other.dream_world.front_default, pokemonType: payload.types[0].type.name, identification: payload.id })))
 }
   render() {
     console.log(this.state.ipPayload)
@@ -24,12 +26,23 @@ getPokemon = () => {
     <>
     <div id="container">
     <h1>Return a Pokemon</h1>
-    <button onClick={this.getPokemon}>Click here for Pokemon</button>
-    <img src='./pokedex.gif' />
-    <p>Your Pokemon is: {this.state.ipPayload.name}
-    </p>
-    <p>{this.state.ipPayload.height}</p>
-    <img src={this.state.imageURL}></img>
+    <button id='pdbutton' onClick={this.getPokemon}>Click here for Pokemon</button>
+    <img id='pdex'src='./pokedex.png' />
+ 
+    {/* errors outputing location */}
+    {/* <p id="pokemonlocation">{this.state.location.location_area_encounters}</p> */}
+
+    <p id="pokemonid">{this.state.identification}</p>
+
+    <p id="pokemonname">{this.state.ipPayload.name}</p>
+    
+    
+    <p id="pokemonetype">{this.state.pokemonType}</p>
+
+    <p id="pokemoneheight">{this.state.ipPayload.height}' tall</p>
+
+    <img id="pokemonimage" src={this.state.imageURL}/>
+
     </div>
     </>
   );
